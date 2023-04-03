c4ff1b8b44e38bb89a7ed79a2ab75af6f2efe0d2
shadow3 <1394025007@qq.com>
2023-04-03 16:39:09 +0000

    add miui

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use fas_framework::{frame::Watcher, misc};
 mod watcher_fbt_info;
 use watcher_fbt_info::FBTWatcher;
 mod mtk_gpu;
-use mtk_gpu::Gpu;
+mod mtk_gpu_miui;
 
 fn main() {
     misc::bound_to_little();
+    let miui = misc::exec_cmd("getprop", &["ro.miui.ui.version.code"]).unwrap().is_empty();
     let watcher_list = [FBTWatcher::give()];
-    let controller_list = [Gpu::give()];
+    let controller_list;
+    if miui {
+        controller_list = [mtk_gpu::Gpu::give()];
+    } else {
+        controller_list = [mtk_gpu_miui::Gpu::give()];
+    }
     let mut w = Watcher::new(&watcher_list, &controller_list);
     w.start();
 }
