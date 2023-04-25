948ddffee3ed8e29acebb6a2c58c4456d029bf35
shadow3 <1394025007@qq.com>
2023-04-25 05:43:05 +0000

    Target Cpu added

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="${0%/*}"
 ELF="$DIR/target/aarch64-unknown-linux-musl/release/fas-framework"
-export RUSTFLAGS="-C target-feature=+crt-static"
+export RUSTFLAGS="-C target-feature=+crt-static -C target-cpu=cortex-a78 -C target-cpu=cortex-a55 -C target-cpu=cortex-x2 -C target-cpu=cortex-a710 -C target-cpu=cortex-a510"
 cargo build --target aarch64-unknown-linux-musl --release
 sstrip "$ELF"
